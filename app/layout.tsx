db4c05da942ba97499e2af41eda7c0e7ff0b47b8
aaron <changxing.wu@gmx.com>
2025-12-18 17:06:27 +1100

    Update version to v0.5.9

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,7 +108,7 @@ const structuredData = {
     "ratingValue": "4.8",
     "ratingCount": "127"
   },
-  "softwareVersion": "0.5.1",
+  "softwareVersion": "0.5.9",
   "softwareHelp": {
     "@type": "WebPage",
     "url": "https://connectonion.com/quickstart"
