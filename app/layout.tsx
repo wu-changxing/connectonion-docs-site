d2615e96234b5151224c1da6aaea3ae4c52477da
aaron <changxing.wu@gmx.com>
2026-02-15 20:14:52 +0800

    Update version to v0.7.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,7 +108,7 @@ const structuredData = {
     "ratingValue": "4.8",
     "ratingCount": "127"
   },
-  "softwareVersion": "0.6.9",
+  "softwareVersion": "0.7.0",
   "softwareHelp": {
     "@type": "WebPage",
     "url": "https://connectonion.com/quickstart"
